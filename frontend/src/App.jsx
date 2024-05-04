139cf74cfb3d45400ba4e5c776632900bb4ea06f
Faraz Majid <farazmajid56@gmail.com>
2024-05-04 17:06:11 +0500

    Bug Fix for Login Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -67,7 +67,7 @@ function App() {
   return (
     <>
       {!isLoggedIn ? (
-        <Login onLogin={toggleisLoggedIn} />
+        <Login contract={contract} onLogin={toggleisLoggedIn} />
       ) : (
         <>
           <NavBar />
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,31 +3,40 @@ import './Login.css';
 
 const Login = ({ contract, accounts, onLogin }) => {
     const [username, setUsername] = useState('');
-    const [existingUser, setexistingUser] = useState(false);
+    const [existingUser, setExistingUser] = useState(false);
 
-    useEffect(() => checkUserExists(), [])
+    useEffect(() => {
+        // Define the async function inside useEffect
+        async function checkUserExists() {
+            if (!window.ethereum) {
+                alert('Please install MetaMask!');
+                return;
+            }
+            const account = await window.ethereum.request({ method: 'eth_requestAccounts' });
+            const isRegistered = await contract.methods.getUser(account[0]).call();
+            if (isRegistered) {
+                setExistingUser(true);
+            } else {
+                setExistingUser(false);
+                // Prompting before setting username might lead to better flow control
+                const name = prompt('Please enter your username for registration:');
+                setUsername(name);
+            }
+        }
+        
+        checkUserExists();
+    }, []); // Dependencies should include all variables used in the effect that could change over time
 
     const connectWallet = async () => {
-        if (window.ethereum) {
-            const account = await window.ethereum.request({ method: 'eth_requestAccounts' });
-            if(!existingUser){
-                await contract.methods.registerUser(username).send({ from: account });
-            }
-            onLogin();
-        } else {
+        if (!window.ethereum) {
             alert('Please install MetaMask!');
+            return;
         }
-    };
-
-    const checkUserExists = async (account) => {
-        const isRegistered = await contract.methods.getUser(account).call();
-        if (isRegistered) {
-            setexistingUser(true);
-        } else {
-            setexistingUser(false);
-            const name = prompt('Please enter your username for registration:');
-            await contract.methods.registerUser(username).send({ from: account });
+        const account = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        if (!existingUser) {
+            await contract.methods.registerUser(username).send({ from: account[0] });
         }
+        onLogin();
     };
 
     return (
@@ -35,12 +44,16 @@ const Login = ({ contract, accounts, onLogin }) => {
             <header className="login-header">
                 Welcome to Decentralized Auction House
             </header>
-            
-            { !existingUser && 
+            {/* TODO: Add check for empty username */}
+            { !existingUser ?  
                 <> 
-                <h1>Please register you account</h1> 
-                <input value={username} className="login-input" onChange={(e) => setUsername(e.target.value)} />
+                    <h1 className="register-heading">Please register your account</h1> 
+                    <label className='login-label'>Username:
+                        <input value={username} className="login-input" onChange={(e) => setUsername(e.target.value)} />
+                    </label>
                 </>
+                :
+                <h1 className="register-heading">Welcomeback</h1> 
             }
 
             <button className="login-button" onClick={connectWallet}>Connect Wallet</button>
@@ -49,7 +62,6 @@ const Login = ({ contract, accounts, onLogin }) => {
                 By Faraz Majid 20L-1162 & Aemon Fatima 20L-1057
             </footer>
         </div>
-    
     );
 };
 

f2f57a7f7203178a4edb9d30f5f1c507c9efad3d
Faraz Majid <farazmajid56@gmail.com>
2024-05-04 21:20:20 +0500

    Added Username & Walletaddres in NavBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,12 +1,26 @@
-import './NavBar.css'
+import './NavBar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
-const NavBar = () => {
+const NavBar = ({ username, walletAddress }) => {
     return (
-        <header className="title-bar">
-            <h1>Decentralized Auction House</h1>
-            <p>By: Faraz Majid 20L-1162 & Aemon Fatima 20L-1057</p>
-        </header>
-    ) 
+        <div className="header-container">
+            <div className="btn-user-auctions">
+                <button>Your Auctions</button>
+            </div>
+            <div className="title-bar">
+                <h1>Decentralized Auction House</h1>
+                <p>By: Faraz Majid 20L-1162 & Aemon Fatima 20L-1057</p>
+            </div>
+            <div className="user-info">
+                <FontAwesomeIcon icon={faUserCircle} size="4x" />
+                <div className="user-details">
+                    <p id="userName">{username}</p>
+                    <p id="walletAdd">{walletAddress}</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default NavBar;
